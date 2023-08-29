d6023b9576adac548273303b70c666dc820f7181
jdiego98 <41030076+jdiego98@users.noreply.github.com>
2023-08-28 19:40:23 -0600

    Initial Commit

---
+++ Infrastructure/Data/StoreContext.cs
@@ -0,0 +1,16 @@
+﻿using api.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace api.Data
+{
+    public class StoreContext : DbContext
+    {
+
+        public StoreContext(DbContextOptions options) : base(options) 
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
